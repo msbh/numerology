87167740825de361cece776150e45e91e1079344
Muhammad Misbah ul Haq Malik <malikmsbh@gmail.com>
2023-01-25 23:57:25 +0500

    Update work.js

--- work.js
+++ work.js
@@ -4,6 +4,11 @@
         'j': 1, 'k': 2, 'l': 3, 'm': 4, 'n': 5, 'o': 6, 'p': 7, 'q': 8, 'r': 9,
         's': 1, 't': 2, 'u': 3, 'v': 4, 'w': 5, 'x': 6, 'y': 7, 'z': 8
       };
+       var KABALA = {
+        'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9,
+        'j': 1, 'k': 2, 'l': 3, 'm': 4, 'n': 5, 'o': 6, 'p': 7, 'q': 8, 'r': 9,
+        's': 1, 't': 2, 'u': 3, 'v': 4, 'w': 5, 'x': 6, 'y': 7, 'z': 8
+      };
 
       var CHALDEAN = {
         'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 8, 'g': 3, 'h': 5, 'i': 1,
@@ -24,6 +29,7 @@
         // Print results
         printResult(PYTHAGOREAN, 'Pythagoras');
         printResult(CHALDEAN, 'Chaldean');
+        printResult(KABALA, 'Kabala');
       }
       
       function checkDesire(comSum,desired,list){
@@ -67,6 +73,11 @@ function sumtoSingleDigit(sum){
 
           // Lookup or convert letter and add to sum
           sum += list[l] || parseInt(l);
+        }
+        if(text=='Kabala'){
+         // Write the sum to screen
+        result.innerHTML += '<div class="result-text"><b>' + text + '</b> </br>' + sum +  '   Sum:   '+ ((sum/9)+1) + '</div>';
+        return;
         }
         if(sum>9)
         { 
